fea007c630b83f6ec27eeb8f807b6ed61644935b
samithaj <samithajayaweera@gmail.com>
2014-06-06 23:17:14 +0530

    asdasd

---
+++ FR/src/fr/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fr;
+
+/**
+ *
+ * @author sam
+ */
+public class NewClass {
+       System.out.println("gsdgsdgsdg");
+        System.out.println("gsdgsdgsdg");
+     
+}
